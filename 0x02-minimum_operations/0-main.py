18cc34054a2d1d16c1a1bcca1667c700086a59b5
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-17 10:05:28 +0300

    0.added parameter checking code

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -5,8 +5,9 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
-n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+while True:
+    n = int(input("Please enter your n: "))
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,6 +10,9 @@ def minOperations(n):
     text editor can execute only two operations in this file copy All and Paste
     give the min num of operations to achieve n times of H
     """
+    if n <= 0:
+        return 0
+
     numberOfOperations = 0
     numberOfPastes = 0
     string = "H"
