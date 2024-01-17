c3dc36aff56191cd958fc9a24629ccab06440b2d
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-17 09:57:48 +0300

    0-minimum operations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+python function that solves minimum operations
+"""
+
+
+def minOperations(n):
+    """
+    a text file there is a single char H your
+    text editor can execute only two operations in this file copy All and Paste
+    give the min num of operations to achieve n times of H
+    """
+    numberOfOperations = 0
+    numberOfPastes = 0
+    string = "H"
+    toBeCopied = string
+
+    while len(string) < n:
+        if numberOfPastes < 2:
+            string += toBeCopied
+            numberOfPastes += 1
+            numberOfOperations += 1
+        else:
+            numberOfPastes = 0
+            toBeCopied = string
+            numberOfOperations += 1
+
+    return numberOfOperations
