c48332f40f032414a649e018a08616505d5cc120
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-25 23:03:15 +0300

    changed code structure

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,53 +1,41 @@
 #!/usr/bin/python3
-"""
-a script that reads stdin line by line and compute metrics
-"""
+
 import sys
-import re
 
 
-if __name__ == "__main__":
-    regex = (
-        r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] '
-        r'"GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)'
-    )
+def print_msg(dict_sc, total_file_size):
+    """print_Msg"""
 
-    numberOfLines = 0
-    validStatus = ['200', '301', '400', '401', '403', '404', '405', '500']
-    pattern = re.compile(r" [0-5]{3} ")
-    statusList = {status: 0 for status in validStatus}
-    totalSize = []
-    sizePattern = re.compile("[0-9]{1,}$")
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
-    try:
-        """ Read stdin line by line """
-        for line in sys.stdin:
-            if not line:
-                break
 
-            if re.match(regex, line):
-                numberOfLines += 1
-                key = pattern.search(line).group().strip()
-                if key in validStatus:
-                    statusList[key] += 1
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0, "301": 0, "400": 0,
+           "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
-                size_match = sizePattern.search(line)
-                if size_match:
-                    totalSize.append(int(size_match.group()))
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
-            if numberOfLines == 10:
-                print("File size:", sum(totalSize))
-                for statusCode, number in statusList.items():
-                    if number == 0:
-                        continue
-                    print(f"{statusCode}: {number}")
-                numberOfLines = 0
+        if len(parsed_line) > 2:
+            counter += 1
 
-    except KeyboardInterrupt:
-        """ handles the exception """
-        print("File size:", sum(totalSize))
-        for statusCode, number in statusList.items():
-            if number == 0:
-                continue
-            print(f"{statusCode}: {number}")
-        raise
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])
+                code = parsed_line[1]
+
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
+
+finally:
+    print_msg(dict_sc, total_file_size)
