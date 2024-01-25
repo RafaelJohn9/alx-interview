a9bceb86ae5dc86d59986a6f598fbfec71b28eb4
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-25 22:47:15 +0300

    0.fixing redundancy in code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,11 +8,10 @@ import re
 
 if __name__ == "__main__":
     regex = (
-            r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] '
-            r'"GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)'
-            )
+        r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] '
+        r'"GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)'
+    )
 
-    line = sys.stdin.readline()
     numberOfLines = 0
     validStatus = ['200', '301', '400', '401', '403', '404', '405', '500']
     pattern = re.compile(r" [0-5]{3} ")
@@ -20,33 +19,34 @@ if __name__ == "__main__":
     totalSize = []
     sizePattern = re.compile("[0-9]{1,}$")
 
-    while line:
-        try:
+    try:
+        while True:
+            line = sys.stdin.readline()
+
+            if not line:
+                break
+
             if re.match(regex, line):
                 numberOfLines += 1
                 key = pattern.search(line).group().strip()
-            if key in validStatus:
-                statusList[key] += 1
+                if key in validStatus:
+                    statusList[key] += 1
 
-            size_match = sizePattern.search(line)
-            if size_match:
-                totalSize.append(int(size_match.group()))
+                size_match = sizePattern.search(line)
+                if size_match:
+                    totalSize.append(int(size_match.group()))
 
             if numberOfLines == 10:
                 print("File size:", sum(totalSize))
-                
                 for statusCode, number in statusList.items():
                     if number == 0:
                         continue
                     print(f"{statusCode}: {number}")
                 numberOfLines = 0
-            line = sys.stdin.readline()
-        except KeyboardInterrupt:
-            print("File size:", sum(totalSize))
-            for statusCode, number in statusList.items():
-                if number == 0:
-                    continue
-                print(f"{statusCode}: {number}")
-            numberOfLines = 0
-            line = sys.stdin.readline()
-            continue
+
+    except KeyboardInterrupt:
+        print("File size:", sum(totalSize))
+        for statusCode, number in statusList.items():
+            if number == 0:
+                continue
+            print(f"{statusCode}: {number}")
