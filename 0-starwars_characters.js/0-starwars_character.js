c7e66a93d8c581c32c77216ef91272be01ce2bc1
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-14 17:45:38 +0300

    0-stawars api

---
+++ 0-starwars_characters.js/0-starwars_character.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+async function fetchData (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+}
+
+async function main () {
+  const movieNumber = parseInt(process.argv[2]);
+  const movieUrl = 'https://swapi-api.alx-tools.com/api/films';
+  const data = await fetchData(movieUrl);
+
+  const movie = data.results[movieNumber];
+
+  for (const character of movie.characters) {
+    const person = await fetchData(character);
+    console.log(person.name);
+  }
+}
+
+main();
