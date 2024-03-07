4b1e3152c2cbdc9377f6800165a869a81d720a7e
RafaelJohn9 <rafaeljohb@gmail.com>
2024-03-07 14:47:17 +0300

    corrected the island_perimeter on the algorithm used

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,58 +3,37 @@
 gets the perimeter of a grid
 """
 
+
 def island_perimeter(grid):
     """
     grid is a 2 x 2 matrix
     """
-    top = lambda co_ord: [co_ord[0] - 1, co_ord[1]]
-    down = lambda co_ord: [co_ord[0] + 1, co_ord[1]]
-    left = lambda co_ord: [co_ord[0], co_ord[1] - 1]
-    right = lambda co_ord: [co_ord[0], co_ord[1] + 1]
+    if not grid:
+        return 0
 
-    visited = []
-    islandCoordinate = []
-
-    for x in range(len(grid)):
-        for y in range(len(grid[0])):
-            if grid[x][y] == 1:
-                islandCoordinate = [x, y]
-                break;
-
-    nextCoordinates = []
     perimeter = 0
+    rows, cols = len(grid), len(grid[0])
 
-    while islandCoordinate not in visited:
-        visited.append(islandCoordinate)
-        determiner = [
-                      top(islandCoordinate),
-                      down(islandCoordinate),
-                      left(islandCoordinate),
-                      right(islandCoordinate)
-                      ]
-        for coordinate in determiner:
-            if coordinate not in visited:
-                x, y = coordinate
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Assume 4 sides initially
 
-                if x < 0 or y >= len(grid[0]) or x >= len(grid):
-                    perimeter += 1
-                    continue
+                # Check neighbors and deduct the shared edges
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
 
-                if grid[x][y] == 1:
-                    nextCoordinates.append([x, y])
-                else:
-                    perimeter += 1
-        if len(nextCoordinates) > 0:
-            islandCoordinate = nextCoordinates.pop()
     return perimeter
 
 
 if __name__ == "__main__":
     grid = [
         [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1],
+        [0, 1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0]
     ]
     print(island_perimeter(grid))
